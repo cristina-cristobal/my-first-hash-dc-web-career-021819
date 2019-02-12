0973594ed8376b19c284108bdf6fded257b115f5
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-12 17:29:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-new_hash = {"name" => "Tina", "age" => 30, "location" => "Arlington" }
+new_hash = {"name" => "Tina", "age" => "30", "location" => "Arlington" }
 end
 new_hash
 end
